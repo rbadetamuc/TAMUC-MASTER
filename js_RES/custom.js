b6ba6ee86e6d5d52879d80a3d87e3825d225a7f5
rbadetamuc <rbade@leomail.tamuc.edu>
2015-06-16 15:23:32 -0500

    latest-copy

---
+++ js_RES/custom.js
@@ -0,0 +1,86 @@
+/* Add here all your JS customizations */
+jQuery(document).ready(function($) {
+	checkSize();
+	$(window).resize(function(){
+   		checkSize();
+	});
+	$('.b-search-box .fa').click(function(){
+		var target = '.b-search-box #searchField';
+		$(target).toggle();
+		if($(target).is(':visible')){
+			$(target).focus();
+		}
+	});
+	/* $( "#requestForm" ).load( "RequestForm.html",function( response, status ) {
+		if ( status) {
+			$('#requestForm').removeClass('hidden');
+	}else{
+		var msg = "Sorry! error loading Request Form. Try reload page";
+			alert(msg);
+	}
+	});  */
+	var gfeedfetcher_loading_image="js/bxslider/images/bx_loader.gif" 
+	$( window ).load(function() {	
+		var slider = $('.j-carousel-rss').bxSlider({
+				maxSlides:2,
+				controls: false,
+				slideMargin: 30,
+				slideWidth: 568,
+				pager: true
+		});
+		if(slider){
+			$('.loadingrss').hide();
+			$('.b-carousel-primary__item').show();	
+			$( "#requestForm" ).load( "RequestForm.html");
+		}
+		
+	});
+		$(window).scroll(function(){
+
+		if ($(this).scrollTop()+$("#scrollObj").height() < $("#scrollObj").offset().top-100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	$(".scrollToTop").click(function(e) {
+		e.preventDefault();
+    	$('html, body').animate({
+        scrollTop: $("#scrollObj").offset().top
+    }, 2000);
+	
+
+});
+	$(".myLeoLink").hover(function(){
+		$(this).find('img').attr('src','img/myLEO-over.svg');
+	},function(){
+		$(this).find('img').attr('src','img/myLEO.svg');		
+	});
+});
+function checkSize(){
+	if ($(window).width() < BREAK.SM) {
+		$('.Parentscroll').show();
+		//$('.foot-widget-area').hide();
+		//$('.foot-widget-title .fa').show();
+	 // Mobile stuff.
+	/* $('.foot-widget-title').on( "click", function(e) {
+		e.preventDefault();
+		$(this).next().slideToggle();
+		$(this).find('.fa').ToggleClass('');
+	}); */
+	}if ($(window).width() > BREAK.SM) {
+		$('.Parentscroll').hide();
+		// $('.foot-widget-area').show();
+		// $('.foot-widget-title .fa').hide();
+		// $('.foot-widget-title').off( "click");
+	}
+	
+}
+function reloadScript()
+{
+    if( event.persisted )
+    {
+          window.location.reload();
+    }
+ 
+}
